b23d68655202cc540a83d9b6748a5760a238aaac
Arnaud Flaesch <arnaudflaesch@hotmail.com>
2016-02-03 12:24:19 +0100

    Affichage de l'article dans le navigateur

--- app/src/main/java/com/jojo/googlenewsreader/Main22Activity.java
+++ app/src/main/java/com/jojo/googlenewsreader/Main22Activity.java
@@ -1,13 +1,26 @@
 package com.jojo.googlenewsreader;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class Main22Activity extends AppCompatActivity {
 
+    private String urlArticle;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main22);
+        Intent intent = getIntent();
+        this.urlArticle = intent.getStringExtra(Main2Activity.urlArticle);
+        WebView webView = (WebView) findViewById(R.id.webView);
+        System.out.println(this.urlArticle);
+        webView.loadUrl(this.urlArticle);
     }
 }
--- app/src/main/java/com/jojo/googlenewsreader/Main2Activity.java
+++ app/src/main/java/com/jojo/googlenewsreader/Main2Activity.java
@@ -3,6 +3,8 @@ package com.jojo.googlenewsreader;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -11,7 +13,8 @@ import org.json.JSONObject;
 
 public class Main2Activity extends AppCompatActivity {
 
-    private JSONObject article;
+    protected JSONObject article;
+    public static String urlArticle;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,5 +31,18 @@ public class Main2Activity extends AppCompatActivity {
         catch (JSONException error) {
 
         }
+        Button button = (Button) findViewById(R.id.openWebView);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(Main2Activity.this, Main22Activity.class);
+                try {
+                    intent.putExtra(urlArticle, article.getString("url"));
+                }
+                catch (JSONException error) {
+
+                }
+                startActivity(intent);
+            }
+        });
     }
 }
