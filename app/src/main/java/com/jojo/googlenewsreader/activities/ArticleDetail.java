59860bdf42b2df37d164963eb1dd7b09b3e58b70
Jordan Marques <jordanmarques1994@gmail.com>
2016-02-21 17:18:34 +0100

    fix bitmap bug

--- app/src/main/java/com/jojo/googlenewsreader/activities/ArticleDetail.java
+++ app/src/main/java/com/jojo/googlenewsreader/activities/ArticleDetail.java
@@ -3,6 +3,7 @@ package com.jojo.googlenewsreader.activities;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.text.Html;
 import android.view.View;
 import android.widget.Button;
@@ -11,6 +12,7 @@ import android.widget.TextView;
 
 import com.jojo.googlenewsreader.R;
 import com.jojo.googlenewsreader.pojo.Article;
+import com.jojo.googlenewsreader.utils.NetworkUtil;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
@@ -42,13 +44,18 @@ public class ArticleDetail extends ParentActivity {
         TextView publisher = (TextView)findViewById(R.id.textView2);
         TextView date = (TextView)findViewById(R.id.textView3);
         TextView url = (TextView)findViewById(R.id.url);
-        ImageView imageViewLandscape = (ImageView)findViewById(R.id.imageViewLandscape);
+        ImageView image = (ImageView)findViewById(R.id.imageViewLandscape);
 
         title.setText(Html.fromHtml(article.getTitle()));
         description.setText(Html.fromHtml(article.getContent()));
         publisher.setText(Html.fromHtml(article.getPublisher()));
         date.setText(Html.fromHtml(article.getDate()));
-        imageViewLandscape.setImageBitmap(image);
+
+        if(NetworkUtil.getConnectivityStatusBoolean(this)){
+            image.setImageBitmap(ArticleDetail.image);
+        } else {
+            image.setBackground(ContextCompat.getDrawable(this, R.drawable.no_image));
+        }
 
 
 
